d3aacf70d613f957f5cb74aac5026bed4dffaf4d
cheolwo <powerqkrcjfd@gmail.com>
2022-01-09 19:59:10 +0900

    .

---
+++ BusinessLogic/ofManagement/KamisManagement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.ofManagement
+{
+    /*
+        string longurl = "http://somesite.com/news.php?article=1&lang=en";
+        var uriBuilder = new UriBuilder(longurl);
+        var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+        query["action"] = "login1";
+        query["attempts"] = "11";
+        uriBuilder.Query = query.ToString();
+        longurl = uriBuilder.ToString();
+// "http://somesite.com:80/news.php?article=1&lang=en&action=login1&attempts=11"
+     */
+    //public class GitHubService
+    //{
+    //    public HttpClient Client { get; }
+
+    //    public GitHubService(HttpClient client)
+    //    {
+    //        client.BaseAddress = new Uri("https://api.github.com/");
+    //        // GitHub API versioning
+    //        client.DefaultRequestHeaders.Add("Accept",
+    //            "application/vnd.github.v3+json");
+    //        // GitHub requires a user-agent
+    //        client.DefaultRequestHeaders.Add("User-Agent",
+    //            "HttpClientFactory-Sample");
+
+    //        Client = client;
+    //    }
+
+    //    public async Task<IEnumerable<GitHubIssue>> GetAspNetDocsIssues()
+    //    {
+    //        return await Client.GetFromJsonAsync<IEnumerable<GitHubIssue>>(
+    //          "/repos/aspnet/AspNetCore.Docs/issues?state=open&sort=created&direction=desc");
+    //    }
+    //}
+    public class KamisManagement
+    {
+
+    }
+}
---
+++ BusinessLogic/ofManager/ofKamis/KamisPartManager.cs
@@ -0,0 +1,124 @@
+using BusinessLogic.ofManager.ofGeneric;
+using BusinessLogic.ofManager.ofGeneric.ofFileFactory;
+using BusinessLogic.ofManager.ofGeneric.ofBlobStorage;
+using System.Threading.Tasks;
+using System;
+using BusinessData.ofCommon.ofKamis.ofModel;
+using BusinessData;
+using BusinessData.ofGeneric.ofIdFactory;
+
+namespace BusinessLoogic.ofManager.ofKamis
+{
+    public class KamisPartManager : EntityManager<KamisPart>
+    {
+        public KamisPartManager(IEntityDataRepository<KamisPart> EntityDataRepository, 
+            IEntityIdFactory<KamisPart> EntityIdFactory,
+            IEntityFileFactory<KamisPart> entityFileFactory, 
+            IEntityBlobStorage<KamisPart> entityBlobStorage, 
+            DicConvertFactory<KamisPart> dicConvertFactory) : base(EntityDataRepository, EntityIdFactory, entityFileFactory, entityBlobStorage, dicConvertFactory)
+        {
+        }
+        public override async Task<KamisPart> CreateAsync(KamisPart entity)
+        {
+            entity.Id = Math.Abs(entity.Name.GetHashCode()).ToString();
+            entity.CreateTime = DateTime.Now;
+            return await _EntityDataRepository.AddAsync(entity);
+        }
+    }
+    public class KamisCommodityManager : EntityManager<KamisCommodity>
+    {
+        public KamisCommodityManager(IEntityDataRepository<KamisCommodity> EntityDataRepository, 
+            IEntityIdFactory<KamisCommodity> EntityIdFactory,
+            IEntityFileFactory<KamisCommodity> entityFileFactory, 
+            IEntityBlobStorage<KamisCommodity> entityBlobStorage, 
+            DicConvertFactory<KamisCommodity> dicConvertFactory) : base(EntityDataRepository, EntityIdFactory, entityFileFactory, entityBlobStorage, dicConvertFactory)
+        {
+        }
+        public override async Task<KamisCommodity> CreateAsync(KamisCommodity entity)
+        {
+            entity.Id = Math.Abs(entity.Name.GetHashCode()).ToString();
+            entity.CreateTime = DateTime.Now;
+            return await _EntityDataRepository.AddAsync(entity);
+        }
+    }
+    public class KamisKindofCommodityManager : EntityManager<KamisKindofCommodity>
+    {
+        public KamisKindofCommodityManager(IEntityDataRepository<KamisKindofCommodity> EntityDataRepository, 
+            IEntityIdFactory<KamisKindofCommodity> EntityIdFactory,
+            IEntityFileFactory<KamisKindofCommodity> entityFileFactory, 
+            IEntityBlobStorage<KamisKindofCommodity> entityBlobStorage, 
+            DicConvertFactory<KamisKindofCommodity> dicConvertFactory) : base(EntityDataRepository, EntityIdFactory, entityFileFactory, entityBlobStorage, dicConvertFactory)
+        {
+        }
+        public override async Task<KamisKindofCommodity> CreateAsync(KamisKindofCommodity entity)
+        {
+            entity.Id = Math.Abs(entity.Name.GetHashCode()).ToString();
+            entity.CreateTime = DateTime.Now;
+            return await _EntityDataRepository.AddAsync(entity);
+        }
+    }
+    public class KamisCountryAdministrationPartManager : EntityManager<KamisCountryAdministrationPart>
+    {
+        public KamisCountryAdministrationPartManager(IEntityDataRepository<KamisCountryAdministrationPart> EntityDataRepository, 
+            IEntityIdFactory<KamisCountryAdministrationPart> EntityIdFactory,
+            IEntityFileFactory<KamisCountryAdministrationPart> entityFileFactory, 
+            IEntityBlobStorage<KamisCountryAdministrationPart> entityBlobStorage, 
+            DicConvertFactory<KamisCountryAdministrationPart> dicConvertFactory) : base(EntityDataRepository, EntityIdFactory, entityFileFactory, entityBlobStorage, dicConvertFactory)
+        {
+        }
+        public override async Task<KamisCountryAdministrationPart> CreateAsync(KamisCountryAdministrationPart entity)
+        {
+            entity.Id = Math.Abs(entity.Name.GetHashCode()).ToString();
+            entity.CreateTime = DateTime.Now;
+            return await _EntityDataRepository.AddAsync(entity);
+        }
+    }
+    public class KamisSubCountryAdministrationPartManager : EntityManager<KamisSubCountryAdministrationPart>
+    {
+        public KamisSubCountryAdministrationPartManager(IEntityDataRepository<KamisSubCountryAdministrationPart> EntityDataRepository, 
+            IEntityIdFactory<KamisSubCountryAdministrationPart> EntityIdFactory,
+            IEntityFileFactory<KamisSubCountryAdministrationPart> entityFileFactory, 
+            IEntityBlobStorage<KamisSubCountryAdministrationPart> entityBlobStorage, 
+            DicConvertFactory<KamisSubCountryAdministrationPart> dicConvertFactory) : base(EntityDataRepository, EntityIdFactory, entityFileFactory, entityBlobStorage, dicConvertFactory)
+        {
+        }
+        public override async Task<KamisSubCountryAdministrationPart> CreateAsync(KamisSubCountryAdministrationPart entity)
+        {
+            entity.Id = Math.Abs(entity.Name.GetHashCode()).ToString();
+            entity.CreateTime = DateTime.Now;
+            return await _EntityDataRepository.AddAsync(entity);
+        }
+    }
+    public class KamisMarketManager : EntityManager<KamisMarket>
+    {
+        public KamisMarketManager(IEntityDataRepository<KamisMarket> EntityDataRepository, 
+            IEntityIdFactory<KamisMarket> EntityIdFactory,
+            IEntityFileFactory<KamisMarket> entityFileFactory, 
+            IEntityBlobStorage<KamisMarket> entityBlobStorage, 
+            DicConvertFactory<KamisMarket> dicConvertFactory) : base(EntityDataRepository, EntityIdFactory, entityFileFactory, entityBlobStorage, dicConvertFactory)
+        {
+        }
+        public override async Task<KamisMarket> CreateAsync(KamisMarket entity)
+        {
+            entity.Id = Math.Abs(entity.Name.GetHashCode()).ToString();
+            entity.CreateTime = DateTime.Now;
+            return await _EntityDataRepository.AddAsync(entity);
+        }
+    }
+    public class KamisDayPriceManager : EntityManager<KamisDayPrice>
+    {
+        public KamisDayPriceManager(IEntityDataRepository<KamisDayPrice> EntityDataRepository, 
+            IEntityIdFactory<KamisDayPrice> EntityIdFactory,
+            IEntityFileFactory<KamisDayPrice> entityFileFactory, 
+            IEntityBlobStorage<KamisDayPrice> entityBlobStorage, 
+            DicConvertFactory<KamisDayPrice> dicConvertFactory) : base(EntityDataRepository, EntityIdFactory, entityFileFactory, entityBlobStorage, dicConvertFactory)
+        {
+        }
+        public override async Task<KamisDayPrice> CreateAsync(KamisDayPrice entity)
+        {
+            entity.Id = Math.Abs(entity.Name.GetHashCode()).ToString();
+            entity.CreateTime = DateTime.Now;
+            return await _EntityDataRepository.AddAsync(entity);
+        }
+    }
+}
