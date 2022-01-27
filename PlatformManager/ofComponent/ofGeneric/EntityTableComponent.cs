1ff1d5fdf8c949dff64eaa72b4ab5e914dc0550b
cheolwo <powerqkrcjfd@gmail.com>
2022-01-27 23:41:34 +0900

    .

--- PlatformManager/ofComponent/ofGeneric/EntityTableComponent.cs
+++ PlatformManager/ofComponent/ofGeneric/EntityTableComponent.cs
@@ -1,249 +1,85 @@
-using BusinessData;
-using BusinessData.ofAccount.ofModel;
-using BusinessData.ofCommon.ofInterface;
-using BusinessLogic.ofManager.ofGeneric;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Identity;
+//using BusinessData;
+//using BusinessData.ofAccount.ofModel;
+//using BusinessData.ofCommon.ofInterface;
+//using BusinessLogic.ofManager.ofGeneric;
+//using Microsoft.AspNetCore.Components;
+//using Microsoft.AspNetCore.Identity;
+//using System.Reflection;
+
 using System.Reflection;
 
 namespace PlatformManager.ofComponent.ofGeneric
 {
     public enum TableManagedMode { Dialog, Page }
     public enum TableViewMode { Get, Detail }
-    public enum TableInitCode { Success, Required_User_Register, Success_With_Table_Setting }
-    public class EntityTableComponent<TEntity> where TEntity : Entity, IRelationable, ITablable, new()
+    public class TableOption
     {
-        protected readonly EntityManager<TEntity> _EntityManager;
-        protected readonly EntityManager<BusinessUser> _BusinessUserManager;
-        protected readonly EntityNavigateFactory<TEntity> _EntityNavigateFactory;
-        protected readonly NavigationManager _NavigationManager;
-        public EntityTableComponent(EntityManager<TEntity> entityManager, EntityManager<BusinessUser> businessUserManager,
-                                EntityNavigateFactory<TEntity> entityNavigateFactory, NavigationManager navigationManager)
+        private TableViewMode TableViewMode { get; set; }
+        private TableManagedMode TableManagedMode { get; set; }
+        private bool IsUserTableSetting { get; set; }
+        public TableOption(TableViewMode tableViewMode, TableManagedMode tableManagedMode)
         {
-            _EntityManager = entityManager;
-            _BusinessUserManager = businessUserManager;
-            _EntityNavigateFactory = entityNavigateFactory;
-            _NavigationManager = navigationManager;
+            TableViewMode = tableViewMode;
+            TableManagedMode = tableManagedMode;
+            IsUserTableSetting = false;
         }
-        private IdentityUser IdentityUser { get; set; }
-        public List<TEntity> Entities { get; set; }
-        private List<PropertyInfo> EntitySingleObject { get; set; }
-        private List<PropertyInfo> EntityMultifleObject { get; set; }
-
-        public string TableViewMode { get; set; }
-        private string TableManagedMode { get; set; }
-        private TableSetting TableSetting { get; set; }
-        private TableInfo EntitySingleObjectTableInfo { get; set; }
-        private List<TableInfo> EntityMultifleObjectTableInfo { get; set; }
-        private Dictionary<string, List<PropertyInfo>> DicTableProp { get; set; }
-        private TEntity Entity = new();
-
-        private bool IsOpenCreateDialog { get; set; }
-        private bool IsOpenUpdateDialog { get; set; }
-        protected bool IsOpenDeleteDialog { get; set; }
-        public string GetTableManagedMode()
+        public void IsSetUserSettingTable() { IsUserTableSetting = true; }
+        public TableOption(string tableView, string tableManagedMode)
         {
-            return TableManagedMode;
+            if (tableView.Equals(TableViewMode.Get.ToString()))
+            {
+                TableViewMode = TableViewMode.Get;
+            }
+            else
+            {
+                TableViewMode = TableViewMode.Detail;
+            }
+            if (tableManagedMode.Equals(TableManagedMode.Dialog.ToString()))
+            {
+                TableManagedMode = TableManagedMode.Dialog;
+            }
+            else
+            {
+                TableManagedMode = TableManagedMode.Page;
+            }
         }
-        public string GetTableViewMode()
+        public TableViewMode GetTableViewMode()
         {
             return TableViewMode;
         }
-        public List<PropertyInfo> GetEntitySingleObjectProps()
+        public TableManagedMode GetTableManagedMode()
         {
-            return EntitySingleObject;
+            return TableManagedMode;
         }
-        public List<PropertyInfo> GetEntityMultifleObjectProps()
+    }
+    public class TableInfo
+    {
+        private PropertyInfo PropertyInfo { get; set; }
+        private Dictionary<string, PropertyInfo> keyValuePairs { get; set; }
+        public TableInfo(PropertyInfo propertyInfo)
         {
-            return EntityMultifleObject;
+            PropertyInfo = propertyInfo;
+            keyValuePairs = new();
         }
-        public TableInfo GetSingObjectTableInfo()
+        public PropertyInfo GetOrigin()
         {
-            return EntitySingleObjectTableInfo;
+            return PropertyInfo;
         }
-        public List<TableInfo> GetMultifleObjectTableInfo()
+        public TableInfo()
         {
-            return EntityMultifleObjectTableInfo;
+            keyValuePairs = new();
         }
-        public async Task<string> OnParametersSetAsync(List<TEntity> entities, UserComponent UserComponent)
+        public void Add(string name, PropertyInfo propertyInfo)
         {
-            if(entities != null)
-            {
-                Entities = entities;
-            }
-            if (UserComponent != null)
-            {
-                IdentityUser = UserComponent.IdentityUser;
-                var BusinessUser = await _BusinessUserManager.GetByUserId(IdentityUser.Id); // 이 코드는 암묵적으로 SNS 계정 하나 당 비즈니스 유저 하나만을 나타낼 수 있음을 의미한다.
-                if (BusinessUser == null) { return TableInitCode.Required_User_Register.ToString(); }
-                TableSetting = BusinessUser.TableSettings.FirstOrDefault(e => e.RelationCode.Equals(Entity.GetRelationCode()));
-                if (TableSetting != null)
-                {
-                    TableManagedMode = TableSetting.ManagedMode;
-                    TableViewMode = TableSetting.ViewMode;
-                    if (TableSetting.GetColumnsSelected == null) { TableSetting.GetColumnsSelected = new(); }
-                    if (TableSetting.DetailColumnsSelected == null) { TableSetting.DetailColumnsSelected = new(); }
-                    return TableInitCode.Success.ToString();
-                }
-                //await InitUserTableSetting(BusinessUser, Entity);
-            }
-            return TableInitCode.Success_With_Table_Setting.ToString();
+            keyValuePairs.Add(name, propertyInfo);
         }
-        private async Task InitUserTableSetting(BusinessUser businessUser, TEntity entity)
+        public IEnumerable<string> GetKeys()
         {
-            // 초기화
-            TableSetting tableSetting = new TableSetting();
-            tableSetting.GetColumnsSelected = new();
-            tableSetting.DetailColumnsSelected = new();
-
-            TableSetting.CreateTime = DateTime.Now;
-            Dictionary<string, List<PropertyInfo>> DicTable = entity.GetToDictionaryforClassifiedPropertyByAttribute();
-            List<string> GetColumnsSelected = new();
-            List<string> DetailColumnsSelected = new();
-            List<PropertyInfo> propertyInfos1 = DicTable[TableMetaInfo.Get];
-            List<PropertyInfo> propertyInfos2 = DicTable[TableMetaInfo.GetMany];
-            List<PropertyInfo> propertyInfos3 = DicTable[TableMetaInfo.GetOne];
-
-            InitGetSettting(propertyInfos1, GetColumnsSelected);
-            InitGetSettting(propertyInfos2, GetColumnsSelected);
-            InitGetSettting(propertyInfos3, GetColumnsSelected);
-
-            List<PropertyInfo> propertyInfos4 = DicTable[TableMetaInfo.Detail];
-            List<PropertyInfo> propertyInfos5 = DicTable[TableMetaInfo.DetailOne];
-            List<PropertyInfo> propertyInfos6 = DicTable[TableMetaInfo.DetailMany];
-
-            InitDetailSettting(propertyInfos4, DetailColumnsSelected);
-            InitDetailSettting(propertyInfos5, DetailColumnsSelected);
-            InitDetailSettting(propertyInfos6, DetailColumnsSelected);
-
-            tableSetting.RelationCode = entity.GetRelationCode();
-            tableSetting.GetColumnsSelected = GetColumnsSelected;
-            tableSetting.DetailColumnsSelected = DetailColumnsSelected;
-
-            await _BusinessUserManager.UpdateAsync(businessUser); // 이거 할 때 오류나면 Attach 로 해야되.
+            return keyValuePairs.Keys;
         }
-        private void InitGetSettting(List<PropertyInfo> propertyInfos, List<string> GetColumnsSelected)
+        public PropertyInfo GetValueByKey(string key)
         {
-            foreach(var prop in propertyInfos)
-            {
-                GetColumnsSelected.Add(prop.Name);
-            }
-        }
-        private void InitDetailSettting(List<PropertyInfo> propertyInfos, List<string> DetailColumnsSelected)
-        {
-            foreach (var prop in propertyInfos)
-            {
-                DetailColumnsSelected.Add(prop.Name);
-            }
-        }
-        public async Task OnInitializedAsync(List<TEntity> entities)
-        {
-            if (entities.Count > 0)
-            {
-                var props = typeof(TEntity).GetProperties();
-                DicTableProp = Entity.GetToDictionaryforClassifiedPropertyByAttribute();
-                InitView();
-                if (IdentityUser != null)
-                {
-                    Entities = await _EntityManager.GetToListByUserAsync(IdentityUser);
-                }
-                else
-                {
-                    Entities = await _EntityManager.GetToListAsync();
-                }
-            }
-        }
-        public void SwitchCreateDialog()
-        {
-            IsOpenCreateDialog = !IsOpenCreateDialog;
-        }
-        public void SwitchDeleteDialog()
-        {
-            IsOpenDeleteDialog = !IsOpenDeleteDialog;
-        }
-        public void SwitchUpdateDialog()
-        {
-            IsOpenUpdateDialog = !IsOpenUpdateDialog;
-        }
-        // PropertyInfo.PropertyType is being
-        private void InitView()
-        {
-            if (TableViewMode.Equals("Get"))
-            {
-                var props = DicTableProp[TableMetaInfo.Get];
-                foreach (var prop in props)
-                {
-                    if (prop.PropertyType.Equals(typeof(IList<>)) || prop.PropertyType.Equals(typeof(List<>)))
-                    {
-                        EntityMultifleObject.Add(prop);
-                    }
-                    else
-                    {
-                        EntitySingleObject.Add(prop);
-                    }
-                }
-                InitEntitySingObject(EntitySingleObject);
-                InitEntityMultifleObject(EntityMultifleObject);
-            }
-            if (TableViewMode.Equals("Detail"))
-            {
-                var props = DicTableProp[TableMetaInfo.Detail];
-                foreach (var prop in props)
-                {
-                    if (prop.PropertyType.Equals(typeof(IList<>)) || prop.PropertyType.Equals(typeof(List<>)))
-                    {
-                        EntityMultifleObject.Add(prop);
-                    }
-                    else
-                    {
-                        EntitySingleObject.Add(prop);
-                    }
-                }
-                InitEntitySingObject(EntitySingleObject);
-                InitEntityMultifleObject(EntityMultifleObject);
-            }
-        }
-        // EntityMultifleObject 에 대한 자료를 제어할 수 있게 해주는 부분.
-        private void InitEntityMultifleObject(List<PropertyInfo> entityMultifleObject)
-        {
-            foreach (var prop in entityMultifleObject)
-            {
-                TableInfo newTable = new TableInfo(prop);
-                var listprops = prop.PropertyType.GetProperties();
-                foreach (var lprop in listprops)
-                {
-                    newTable.Add(lprop.Name, lprop);
-                }
-                EntityMultifleObjectTableInfo.Add(newTable);
-            }
-        }
-        private void InitEntitySingObject(List<PropertyInfo> oneProps)
-        {
-            TableInfo newTable = new TableInfo();
-            foreach (var prop in oneProps)
-            {
-                newTable.Add(prop.Name, prop);
-            }
-            EntitySingleObjectTableInfo = newTable;
-        }
-        public async Task CreateAsync(TEntity entity)
-        {
-            // 이러한 코드 부분은 Finallty 에 해당한다.
-            await _EntityManager.CreateAsync(entity);
-            SwitchCreateDialog();
-            _NavigationManager.NavigateTo(_EntityNavigateFactory.GetPageofEntity(entity));
-        }
-        public async Task UpdateAsync(TEntity entity)
-        {
-            await _EntityManager.UpdateAsync(entity);
-            SwitchUpdateDialog();
-            _NavigationManager.NavigateTo(_EntityNavigateFactory.GetPageofEntity(entity));
-        }
-        public async Task DeleteAsync(TEntity entity)
-        {
-            await _EntityManager.DeleteByIdAsync(entity.Id);
-            SwitchDeleteDialog();
-            _NavigationManager.NavigateTo(_EntityNavigateFactory.GetPageofEntity(entity));
+            return keyValuePairs[key];
         }
     }
 }
