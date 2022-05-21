bd568e889a7dea9d9e9483c7a49190dfb51eddbd
cheolwo <powerqkrcjfd@gmail.com>
2022-05-21 22:36:22 +0900

    .

---
+++ WebApp/ofEmployer/EmployerLogisterWebApp/wwwroot/scripts.js
@@ -0,0 +1,62 @@
+﻿var item = "Type anything here";
+export function showPrompt(message) {
+    return prompt(message, item);
+}
+export function init() {
+	var url = location.href;
+	var confmKey = "devU01TX0FVVEgyMDIyMDUyMTE5NTExNjExMjU5Njc=";
+	var resultType = "4"; // 도로명주소 검색결과 화면 출력내용, 1 : 도로명, 2 : 도로명+지번+상세보기(관련지번, 관할주민센터), 3 : 도로명+상세보기(상세건물명), 4 : 도로명+지번+상세보기(관련지번, 관할주민센터, 상세건물명)
+	var inputYn = "<%=inputYn%>";
+	if (inputYn != "Y") {
+		document.form.confmKey.value = confmKey;
+		document.form.returnUrl.value = url;
+		document.form.resultType.value = resultType;
+		document.form.action = "https://www.juso.go.kr/addrlink/addrLinkUrl.do"; //인터넷망
+		//document.form.action="https://www.juso.go.kr/addrlink/addrMobileLinkUrl.do"; //모바일 웹인 경우, 인터넷망
+		document.form.submit();
+	} else {
+		opener.jusoCallBack("<%=roadFullAddr%>", "<%=roadAddrPart1%>", "<%=addrDetail%>", "<%=roadAddrPart2%>", "<%=engAddr%>", "<%=jibunAddr%>", "<%=zipNo%>", "<%=admCd%>", "<%=rnMgtSn%>", "<%=bdMgtSn%>", "<%=detBdNmList%>", "<%=bdNm%>", "<%=bdKdcd%>", "<%=siNm%>", "<%=sggNm%>", "<%=emdNm%>", "<%=liNm%>", "<%=rn%>", "<%=udrtYn%>", "<%=buldMnnm%>", "<%=buldSlno%>", "<%=mtYn%>", "<%=lnbrMnnm%>", "<%=lnbrSlno%>", "<%=emdNo%>");
+		window.close();
+	}
+}
+
+export function goPopup() {
+	// 주소검색을 수행할 팝업 페이지를 호출합니다.
+	// 호출된 페이지(jusopopup.jsp)에서 실제 주소검색URL(https://www.juso.go.kr/addrlink/addrLinkUrl.do)를 호출하게 됩니다.
+	var pop = window.open("/popup/jusoPopup.jsp", "pop", "width=570,height=420, scrollbars=yes, resizable=yes");
+
+	// 모바일 웹인 경우, 호출된 페이지(jusopopup.jsp)에서 실제 주소검색URL(https://www.juso.go.kr/addrlink/addrMobileLinkUrl.do)를 호출하게 됩니다.
+	//var pop = window.open("/popup/jusoPopup.jsp","pop","scrollbars=yes, resizable=yes"); 
+}
+
+function jusoCallBack(roadFullAddr, roadAddrPart1, addrDetail, roadAddrPart2, engAddr, jibunAddr, zipNo, admCd, rnMgtSn, bdMgtSn, detBdNmList, bdNm, bdKdcd, siNm, sggNm, emdNm, liNm, rn, udrtYn, buldMnnm, buldSlno, mtYn, lnbrMnnm, lnbrSlno, emdNo) {
+	// 팝업페이지에서 주소입력한 정보를 받아서, 현 페이지에 정보를 등록합니다.
+	document.form.roadFullAddr.value = roadFullAddr;
+	document.form.roadAddrPart1.value = roadAddrPart1;
+	document.form.roadAddrPart2.value = roadAddrPart2;
+	document.form.addrDetail.value = addrDetail;
+	document.form.engAddr.value = engAddr;
+	document.form.jibunAddr.value = jibunAddr;
+	document.form.zipNo.value = zipNo;
+	document.form.admCd.value = admCd;
+	document.form.rnMgtSn.value = rnMgtSn;
+	document.form.bdMgtSn.value = bdMgtSn;
+	document.form.detBdNmList.value = detBdNmList;
+	/** 2017년 2월 추가제공 **/
+	document.form.bdNm.value = bdNm;
+	document.form.bdKdcd.value = bdKdcd;
+	document.form.siNm.value = siNm;
+	document.form.sggNm.value = sggNm;
+	document.form.emdNm.value = emdNm;
+	document.form.liNm.value = liNm;
+	document.form.rn.value = rn;
+	document.form.udrtYn.value = udrtYn;
+	document.form.buldMnnm.value = buldMnnm;
+	document.form.buldSlno.value = buldSlno;
+	document.form.mtYn.value = mtYn;
+	document.form.lnbrMnnm.value = lnbrMnnm;
+	document.form.lnbrSlno.value = lnbrSlno;
+	/** 2017년 3월 추가제공 **/
+	document.form.emdNo.value = emdNo;
+
+}
